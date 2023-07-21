279543835bd977064f4c7c6f24adb302a2cef856
Cody Slivinski <littleski92@gmail.com>
2023-07-21 16:19:46 -0500

    moved git to root

---
+++ SAT_Project.UI.MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SAT_Project.UI.MVC.Models;
+
+namespace SAT_Project.UI.MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SAT_Project.UI.MVC.Models.ContactViewModel>? ContactViewModel { get; set; }
+    }
+}
